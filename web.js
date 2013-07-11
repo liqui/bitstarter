e4a04dfd787d0786cc967a50fd9a994c16ddc8be
liqui <sakis89@gmail.com>
2013-07-11 04:22:00 -0700

    Updated web.js file

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-
 app.get('/', function(request, response) {
   var buffer = new Buffer(fs.readFileSync(index.html))
   response.send('buffer.toString('utf-8');
